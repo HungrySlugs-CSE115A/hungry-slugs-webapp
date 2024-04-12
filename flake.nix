8a7b2ca94ad5b0943ce8ca6ffafc92e898ee11f8
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-12 15:10:50 -0700

    updated dev environment

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
   };
 
   outputs = {
     systems,
     nixpkgs,
+    devenv,
     ...
   } @ inputs: let
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
@@ -18,20 +20,33 @@
         config.allowUnfree = true;
       };
     in {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs
-          # You can set the major version of Node.js to a specific one instead
-          # of the default version
-          # pkgs.nodejs-19_x
+      default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          { # https://devenv.sh/reference/options/
+            packages = [
+              pkgs.nodejs
+              # You can set the major version of Node.js to a specific one instead of the default version
+              # pkgs.nodejs-19_x
 
-          # You can choose pnpm, yarn, bun, or none (npm).
-          # pkgs.nodePackages.pnpm
-          pkgs.bun
-          # pkgs.yarn
+              # You can choose pnpm, yarn, bun, or none (npm).
+              # pkgs.nodePackages.pnpm
+              # pkgs.yarn
+              pkgs.bun
 
-          pkgs.typescript
-          pkgs.nodePackages.typescript-language-server
+              pkgs.typescript
+              pkgs.nodePackages.typescript-language-server
+            ];
+
+            # Python environment
+            languages.python = {
+              enable = true;
+              venv = {
+                enable = true;
+                requirements = ./requirements.txt;
+              };
+            };
+          }
         ];
       };
     });
