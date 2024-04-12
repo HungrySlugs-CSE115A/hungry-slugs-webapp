e7003a7da1be0d534e9b3b5e412df306a82fa8c3
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-12 15:33:07 -0700

    Remove unused Django files and update .gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,9 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from ast import Is
 from pathlib import Path
-from manage import DJANGO_SECRET_KEY, IS_DEV
+import os
+import sys
+
+parentDir = os.path.dirname(os.path.abspath(__file__))
+secondParentDir = os.path.dirname(parentDir)
+thirdParentDir = os.path.dirname(secondParentDir)
+sys.path.append(thirdParentDir)
+
+from private.private_settings import DJANGO_SECRET_KEY, IS_DEV
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,8 @@
 import os
 import sys
 
-
 parentDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parentDir)
-
 from private.private_settings import DJANGO_SECRET_KEY, IS_DEV
 
 
