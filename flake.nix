f9042ef47ffb5c02124d7397aeb1bac3a607c12a
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-14 23:04:07 -0700

    updated nix dev env

--- flake.nix
+++ flake.nix
@@ -1,30 +1,74 @@
+# {
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#     systems.url = "github:nix-systems/default";
+#   };
+#   outputs = {nixpkgs, systems, ...}: let
+#     forEachSystem = nixpkgs.lib.genAttrs (import systems);
+#   in
+#     forEachSystem(system:
+#         let
+#           pkgs = import nixpkgs {
+#             inherit system;
+#           };
+#         in
+#         {
+#           formatter = pkgs.nixpkgs-fmt;
+
+#           # devShells.default = pkgs.mkShell {
+#           #   buildInputs = [
+#           #     pkgs.nodejs
+#           #     # You can set the major version of Node.js to a specific one instead of the default version
+#           #     # pkgs.nodejs-19_x
+
+#           #     # You can choose pnpm, yarn, bun, or none (npm).
+#           #     # pkgs.nodePackages.pnpm
+#           #     # pkgs.yarn
+#           #     pkgs.bun
+
+#           #     pkgs.typescript
+#           #     pkgs.nodePackages.typescript-language-server
+#           #   ];
+
+#           #   shellHook = ''
+#           #     # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+#           #     # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+#           #     export PIP_PREFIX=$(pwd)/_build/pip_packages
+#           #     export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+#           #     export PATH="$PIP_PREFIX/bin:$PATH"
+#           #     unset SOURCE_DATE_EPOCH
+#           #   '';
+#           # };
+#           devShells.default = pkgs.mkShell {};
+#         }
+#       );
+# }
+
+
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
   };
-
-  outputs = {
-    systems,
-    nixpkgs,
-    devenv,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    devShells = forEachSystem (system: let
-      # Define the packages for the given system
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          { # https://devenv.sh/reference/options/
-            packages = [
+  outputs = { nixpkgs, systems, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (system: {
+        default =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+            my-python = pkgs.python3.withPackages (ps: with ps; [
+              black
+              pip
+            ]);
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              # JS/TS
               pkgs.nodejs
               # You can set the major version of Node.js to a specific one instead of the default version
               # pkgs.nodejs-19_x
@@ -36,19 +80,21 @@
 
               pkgs.typescript
               pkgs.nodePackages.typescript-language-server
+
+              # Python
+              my-python
             ];
 
-            # Python environment
-            languages.python = {
-              enable = true;
-              venv = {
-                enable = true;
-                requirements = ./requirements.txt;
-              };
-            };
-          }
-        ];
-      };
-    });
-  };
+            shellHook = ''
+              # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+              # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+              export PIP_PREFIX=$(pwd)/_build/pip_packages
+              export PYTHONPATH="$PIP_PREFIX/${my-python.sitePackages}:$PYTHONPATH"
+              export PATH="$PIP_PREFIX/bin:$PATH"
+              unset SOURCE_DATE_EPOCH
+              pip install -r requirements.txt
+            '';
+          };
+      });
+    };
 }
