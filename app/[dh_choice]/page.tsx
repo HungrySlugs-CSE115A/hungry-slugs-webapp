7861835f1fbe91af5d6439c5607324afd5c3bf76
Noahkmm <noahkimbot@gmail.com>
2024-04-17 13:40:47 -0700

    made dynamic pages to dining halls

---
+++ app/[dh_choice]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+import { useSearchParams } from "next/navigation";
+import { stringify } from "querystring";
+import React from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Meals{
+  "Breakfast": Array<string>;
+  "Lunch": Array<string>;
+  "Dinner": Array<string>;
+  "Late Night": Array<string>;
+}
+
+export default function Page({searchParams, }:{searchParams:{
+  name: string;
+  //meals: Meals;
+};
+}) {
+  const [meals, set_meals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/myapi/dining-halls/")
+      .then((response) => {
+        const dhs = response.data["Dining Halls"];
+        const a_dh = dhs[0]; 
+        const meal_time = a_dh["meals"];
+        const breakfast = meal_time["Dinner"];
+
+        set_meals(breakfast);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  
+  const test = ["test", "fuck"];
+  //const foods = searchParams.meals;
+
+
+  return (
+
+    <main>
+      <div>
+        <h2 className="text-xl">Welcome to dining hall: {searchParams.name}</h2> 
+ 
+        <p>Breakfast Meals:</p>
+        <ul>
+          {meals.map((meal, i) => (
+            <li key={meal[i]}>{meal}</li>
+          ))}
+        </ul>
+      </div>
+    </main> //create button to go to each dning hall hvac to pass data through button
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,56 @@
 "use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Link from "next/link"
+import React from 'react';
+
+interface Meals {
+  "Breakfast": Array<string>;
+  "Lunch": Array<string>;
+  "Dinner": Array<string>;
+  "Late Night": Array<string>;
+}
+
+interface DiningHall {
+  name: string;
+  meals: Meals;
+
+}
+
+
+function ButtonLink(props: any) {
+  return (
+    <div className="underline text-blue-600">
+      <Link href={{
+        pathname: `/${encodeURIComponent(props.number)}`,
+        query: {
+          name: props.name
+        }
+      }}>{props.button_name}</Link>
+    </div>
+  );
+}
+
 
 export default function Home() {
+  const [dhs_names, set_dhs] = useState([]);
   const [dh_name, set_dh_name] = useState("");
   const [meals, set_meals] = useState([]);
+
+
+  <button id="myButton" className="float-left submit-button" >Home</button>
+
 
   useEffect(() => {
     axios
       .get("http://localhost:8000/myapi/dining-halls/")
       .then((response) => {
-        const dhs = response.data["Dining Halls"];
+        const dhs: Array<DiningHall> = response.data["Dining Halls"];
+        const result: string[] = [];
+        dhs.forEach((value: DiningHall, index: number) => {
+          result.push(dhs[index].name);
+        })
+        set_dhs(result);
         const a_dh = dhs[0];
         set_dh_name(a_dh["name"]);
         const meal_time = a_dh["meals"];
@@ -23,17 +63,29 @@ export default function Home() {
       });
   }, []);
 
+
   return (
     <main>
       <div>
         <h1 className="text-8xl">Welcome to Hungry Slugs!</h1>
-        <h2 className="text-xl">name: {dh_name}</h2>
-        <p>Breakfast Meals:</p>
+
+        <ul>
+
+          {dhs_names.map((meal, i) => (
+            <li>
+              <ButtonLink button_name={dhs_names[i]} name={dhs_names[i]} />
+            </li>
+          ))}
+
+        </ul>
+
+        {/* <h2 className="text-xl">name: {dh_name}</h2> //this was the old means of displaying meals
+        <p>Breakfast Meals:</p>                          // a version of it exists in the dh_choice page
         <ul>
           {meals.map((meal, i) => (
             <li key={meal[i]}>{meal}</li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </main>
   );
