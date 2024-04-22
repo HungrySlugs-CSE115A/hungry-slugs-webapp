3186460245159df989633714a62d4ff0bafc68da
anya zhang <azhang97@ucsc.edu>
2024-04-21 19:36:54 -0700

    use nextjs and committing on right branch

---
+++ app/cowell_stevenson/page.jsx
@@ -0,0 +1,7 @@
+export default function Cowell_Stevenson() {
+  return (
+    <main>
+      <h1>Cowell and Stevenson Menu</h1>
+    </main>
+  ) 
+}
---
+++ app/rcc_oakes/page.jsx
@@ -0,0 +1,7 @@
+export default function RCC_Oakes() {
+  return (
+    <main>
+      <h1>Rachel Carson and Oakes Menu</h1>
+    </main>
+  ) 
+}
