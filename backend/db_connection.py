8705516083ac21174e8457cd6e4d8544ce36506c
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-22 20:13:31 -0700

    change db collection name

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -3,7 +3,7 @@ from utils import get_db_handle, get_local_db_handle
 from private.private_settings import MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_CLUSTER, IS_DEV
 
 if IS_DEV and False: # Remove the False to test locally but you will have to have a local mongodb server running
-    db, _ = get_local_db_handle("test")
+    db, _ = get_local_db_handle("webapp_backend")
 else:
-    db, _ = get_db_handle("test", MONGODB_PASSWORD, MONGODB_USERNAME, MONGODB_CLUSTER)
+    db, _ = get_db_handle("webapp_backend", MONGODB_PASSWORD, MONGODB_USERNAME, MONGODB_CLUSTER)
 
