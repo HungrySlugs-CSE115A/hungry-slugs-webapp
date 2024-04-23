d909b72c112cb6c28d51a9a04e747ffbd13b6353
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-22 19:26:37 -0700

    create backend functions to upload to database

--- backend/myapi/views.py
+++ backend/myapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 
 from .webscraper.food_options import FoodOptions
 
+from .db_functions.dining_halls import get_all_dining_halls_from_db
 
 # Create your views here.
 @api_view(["GET"])
@@ -13,7 +14,14 @@ def hello_world(request):
 # Get the list of dining halls
 @api_view(["GET"])
 def get_dining_halls(request):
-    fo = FoodOptions()
-    json_data = fo.jsonify()
+    # Get all dining halls from the db
+    dining_halls : list[dict] = get_all_dining_halls_from_db()
+
+    # remove the _id field from each dining hall
+    for dh in dining_halls:
+        dh.pop("_id")
+
+    # Convert the list of dining halls to json
+    json_data = {"dining_halls": dining_halls}
 
     return Response(json_data)
