e3367959c213f5e4ee2971b1663ad3c60f9ecc64
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-22 19:27:20 -0700

    update frontend to use new backend

--- app/[dh_choice]/page.tsx
+++ app/[dh_choice]/page.tsx
@@ -6,17 +6,18 @@ import React from "react"
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-interface Meals{
+interface Meals {
   "Breakfast": Array<string>;
   "Lunch": Array<string>;
   "Dinner": Array<string>;
   "Late Night": Array<string>;
 }
 
-export default function Page({searchParams, }:{searchParams:{
-  name: string;
-  //meals: Meals;
-};
+export default function Page({ searchParams, }: {
+  searchParams: {
+    name: string;
+    //meals: Meals;
+  };
 }) {
   const [meals, set_meals] = useState([]);
 
@@ -24,8 +25,8 @@ export default function Page({searchParams, }:{searchParams:{
     axios
       .get("http://localhost:8000/myapi/dining-halls/")
       .then((response) => {
-        const dhs = response.data["Dining Halls"];
-        const a_dh = dhs[0]; 
+        const dhs = response.data["dining_halls"];
+        const a_dh = dhs[0];
         const meal_time = a_dh["meals"];
         const breakfast = meal_time["Dinner"];
 
@@ -35,7 +36,7 @@ export default function Page({searchParams, }:{searchParams:{
         console.log(error);
       });
   }, []);
-  
+
   const test = ["test", "fuck"];
   //const foods = searchParams.meals;
 
@@ -44,15 +45,15 @@ export default function Page({searchParams, }:{searchParams:{
 
     <main>
       <div>
-        <h2 className="text-xl">Welcome to dining hall: {searchParams.name}</h2> 
- 
+        <h2 className="text-xl">Welcome to dining hall: {searchParams.name}</h2>
+
         <p>Breakfast Meals:</p>
         <ul>
           {meals.map((meal, i) => (
-            <li key={meal[i]}>{meal}</li>
+            <li key={i}>{meal}</li>
           ))}
         </ul>
       </div>
     </main> //create button to go to each dning hall hvac to pass data through button
-    );
+  );
 }
