cfe2b6f79b89a2b41d739af289ff937137684566
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-29 21:10:22 -0700

    slight update to flake.nix

--- flake.nix
+++ flake.nix
@@ -36,14 +36,12 @@
 
               # Python
               my-python
-
-              pkgs.krb5
             ];
 
             shellHook = ''
               # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
               # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
-              export PIP_PREFIX=$(pwd)/_build/pip_packages
+              export PIP_PREFIX=$(${pkgs.coreutils}/bin/pwd)/_build/pip_packages
               export PYTHONPATH="$PIP_PREFIX/${my-python.sitePackages}:$PYTHONPATH"
               export PATH="$PIP_PREFIX/bin:$PATH"
               unset SOURCE_DATE_EPOCH
