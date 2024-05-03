1cb21b38447a8eb0d0069eebca2582ed2d1a57b7
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-02 19:37:49 -0700

    updated backend and frontend with new webscraper

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,23 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
 
+interface Food {
+  name: string;
+}
+
+interface subCategory {
+  name: string;
+  foods: Array<Food>;
+}
+
+interface Category {
+  name: string;
+  sub_categories: Array<subCategory>;
+}
+
 interface DiningHall {
   name: string;
-  meals: any;
+  categories: Category;
 }
 
 function ButtonLink(props: any) {
@@ -37,19 +51,19 @@ export default function Home() {
       .get("http://localhost:8000/myapi/dining-halls/")
       .then((response) => {
         // get the data from the response
-        const dhs: Array<DiningHall> = response.data["dining_halls"];
+        const dhs: Array<DiningHall> = response.data["locations"];
 
         // print the data to the console
         console.log(dhs);
 
         // extract the names of the dining halls
-        const dhs_names_temp: string[] = [];
-        dhs.forEach((value: DiningHall) => {
-          dhs_names_temp.push(value.name);
+        const dhs_names: string[] = [];
+        dhs.forEach((dh: DiningHall) => {
+          dhs_names.push(dh["name"]);
         });
 
         // set the state of the dining hall names
-        set_dhs_names(dhs_names_temp);
+        set_dhs_names(dhs_names);
       })
       .catch((error) => {
         console.log(error);
