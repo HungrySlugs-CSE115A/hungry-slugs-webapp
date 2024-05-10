b97492d692631d8b743d325854be99bd6e66d61f
AkhilS2 <55607137+AkhilS2@users.noreply.github.com>
2024-05-09 20:05:48 -0700

    revert backend

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path("hello-world/", views.hello_world, name="hello_world"),
     path("locations/", views.get_locations, name="locations"),
-    #path('users/', views.create_user, name='create_user'),
-    path('users/<str:backend>/', views.create_user, name='create_user'),
-    #path('getuser/', views.get_user, name='get_user'),
 ]
