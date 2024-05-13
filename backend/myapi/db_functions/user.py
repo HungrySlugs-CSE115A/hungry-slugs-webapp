747fdb3a18b71a1645e34f29c6233625dac6eea3
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-12 21:06:51 -0700

    make user model follow crud

--- backend/myapi/db_functions/user.py
+++ backend/myapi/db_functions/user.py
@@ -1,3 +1,4 @@
+from requests import delete
 from ..models import users_collection
 
 """
@@ -8,8 +9,7 @@ ratings: {
 }
 """
 
-# user
-# rating
+## Basic CRUD
 
 def set_user(username: str, id: str) -> None:
     # check if user already exists
@@ -19,38 +19,22 @@ def set_user(username: str, id: str) -> None:
     # add user
     users_collection.insert_one({"username": username, "id": id, "ratings": {}})
 
-
 def get_user(id: str) -> dict | None:
     return users_collection.find_one({"id": id})
 
-def get_user_by_username(username: str) -> dict[str, str|dict] | None:
-    return users_collection.find_one({"username": username})
-
-def set_food_rating(iL: str, food_name: str, rating: int) -> None:
+def update_user(id: str, food_name: str, rating: int|None=None) -> None:
     # check if user exists
     user = users_collection.find_one({"id": id})
     if not user:
         return
-    # add/change rating
-    user["ratings"][food_name] = rating
-    # update user
-    users_collection.update_one(
-        {"id": id}, {"$set": {"ratings": user["ratings"]}}
-    )
 
-def get_food_rating(id: str, food_name: str) -> int | None:
-    # check if user exists
-    user = users_collection.find_one({"id": id})
-    if not user:
-        return None
-    # check if user rated
-    if food_name not in user["ratings"]:
-        return None
-    return user["ratings"][food_name]
+    if rating is not None:
+        # add/change rating
+        user["ratings"][food_name] = rating
+        # update user
+        users_collection.update_one(
+            {"id": id}, {"$set": {"ratings": user["ratings"]}}
+        )
 
-def get_food_ratings(id: str) -> dict[str, int] | None:
-    # check if user exists
-    user = users_collection.find_one({"id": id})
-    if not user:
-        return None
-    return user["ratings"]
+def delete_user(id: str) -> None:
+    users_collection.delete_one({"id": id})
