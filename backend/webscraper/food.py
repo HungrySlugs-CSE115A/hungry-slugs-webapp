c7c10a59dc0fdc3be33e1f106be906d059d5110f
Noahkmm <135295197+Noahkmm@users.noreply.github.com>
2024-05-13 23:10:26 -0700

    Update food.py variable for allergy data
    Changed dict key to "allergies"

--- backend/webscraper/food.py
+++ backend/webscraper/food.py
@@ -26,4 +26,4 @@ class Food:
 
     def to_dict(self) -> dict:
         # foodObj = {self.name: self.allergies}
-        return {"name": self.name, self.name: self.allergies}
+        return {"name": self.name, "allergies": self.allergies}
