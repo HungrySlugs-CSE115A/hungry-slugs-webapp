f41c42e006c0427231f7412b68cf048cb2d7c485
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-16 19:04:27 -0700

    remove old import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/navbar";
-import Testbar from "@/components/testbar";
 
 const monst = Montserrat({ subsets: ["latin"] });
 
