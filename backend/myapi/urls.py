9ce8a71cc4dba90eab19a917eacba79f6c1305b0
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-16 17:49:43 -0700

    add url for backend food fetch

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .model_logic.foods import views as foods_views
 
 urlpatterns = [
     path("locations/", views.get_locations, name="locations"),
-    path("foods/<str:food_name>/", views.get_food, name="foods"),
+    path("foods/<str:name>/", foods_views.get_food, name="foods"),
 ]
