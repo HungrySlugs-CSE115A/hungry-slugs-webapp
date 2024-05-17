133d0c5d55a630e89b4fb97d1e8ac34ea6433cdf
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-16 18:04:04 -0700

    add backend function to frontend

--- app/foods/[food]/page.tsx
+++ app/foods/[food]/page.tsx
@@ -1,8 +1,23 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Page({ params }: { params: { food: string } }) {
-  useEffect;
+export default function Page({ params }: { params: { food: string }}) {
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/myapi/foods/${encodeURIComponent(params.food)}/`)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+        }
+        else {
+          console.error(response);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [params.food]);
 
   return (
     <div>
