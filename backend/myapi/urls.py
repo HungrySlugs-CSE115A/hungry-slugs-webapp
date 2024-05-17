a0cec7d5ebd7fd0f19b4c6402270b187afebb195
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-16 19:04:49 -0700

    remove function in developement

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("locations/", views.get_locations, name="locations"),
     path("users", views.validate_user, name="users"),
     path("logout/", views.current_logout, name="current_logout"),
-    path("foods/<str:food_name>/", views.get_food, name="foods"),
+    # path("foods/<str:food_name>/", views.get_food, name="foods"),
 ]
