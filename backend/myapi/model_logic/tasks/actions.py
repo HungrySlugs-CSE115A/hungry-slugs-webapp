b3c083974c6fa4c3eb6e7fc9d1af0c7113be15ff
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-19 13:42:21 -0700

    updated tasks to follow utils

--- backend/myapi/model_logic/tasks/actions.py
+++ backend/myapi/model_logic/tasks/actions.py
@@ -1,9 +1,7 @@
 from requests import get
 from ...models import tasks_collection
 from datetime import datetime
-
-# import time from django
-from django.utils import timezone
+from utils import get_date_str, str_to_datetime
 
 """
 name: str
@@ -18,13 +16,10 @@ def set_task(task_name: str) -> dict[str, str]:
     Create a new task with the given name
     """
     # get the time from django and make it naive
-    time_now: datetime = timezone.now().replace(tzinfo=None)
-
-    # convert the time to a string
-    time_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
+    time_now: str = get_date_str()
 
     # create a new task
-    task = {"name": task_name, "last_update": time_str}
+    task = {"name": task_name, "last_update": time_now}
 
     # insert the task into the tasks collection
     tasks_collection.insert_one(task)
@@ -39,12 +34,11 @@ def get_task(task_name: str) -> dict | None:
     return tasks_collection.find_one({"name": task_name})
 
 
-def update_task(task_name: str, last_update: datetime) -> None:
+def update_task(task_name: str) -> None:
     """
     Update the task in the database
     """
-    # convert the time to a string
-    time_str = last_update.strftime("%Y-%m-%d %H:%M:%S")
+    time_str = get_date_str()
 
     # update the task
     tasks_collection.update_one(
@@ -73,20 +67,14 @@ def get_last_update_time(task_name: str) -> datetime | None:
     if task is None:
         return None
 
+    # check if the last update time exists
+    if "last_update" not in task:
+        return None
+
     # get the last update time
     last_update = task["last_update"]
 
     # convert the string to a datetime object
-    last_update_time = datetime.strptime(last_update, "%Y-%m-%d %H:%M:%S")
+    last_update_time = str_to_datetime(last_update)
 
     return last_update_time
-
-
-## helper functions
-
-
-def str_to_datetime(time_str: str) -> datetime:
-    """
-    Convert a string to a datetime object
-    """
-    return datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
