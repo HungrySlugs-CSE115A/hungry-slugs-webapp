92cb212ed7e8fb2be8744fcbf0bbc7bb6a2baa9a
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-19 17:19:30 -0700

    add location interface

--- app/locations/[location]/page.tsx
+++ app/locations/[location]/page.tsx
@@ -1,27 +1,9 @@
 "use client";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import LocationFood from "@/components/location/food";
 
-interface Food {
-  name: string;
-  restrictions: Array<string>;
-}
-
-interface subCategory {
-  name: string;
-  foods: Array<Food>;
-}
-
-interface Category {
-  name: string;
-  sub_categories: Array<subCategory>;
-}
-
-interface Location {
-  name: string;
-  categories: Category[];
-}
+import { Location } from "@/interfaces/Location";
 
 export default function Page({ params }: { params: { location: number } }) {
   const [location, setLocation] = useState<Location>();
